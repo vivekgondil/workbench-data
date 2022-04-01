b503e24b726cf97606c38fe21b72853373009a54
VivekG-Osmosys <vivek.g@osmosys.co>
2022-04-01 18:34:51 +0530

    removed files

--- ReImport/snapshots.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-    "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/core_details/sources/argos_uk/snapshots/6e4435e1-c29f-58ea-a456-cd1dc5a4bbd2/home",
-    "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/core_details/sources/apteka_ru/snapshots/a00c5e27-7203-5df7-9190-9664c0d41c32/home"
-]
