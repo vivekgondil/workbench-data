ce1a640c8cce91e46c20cd639f59c45f2137b266
VivekG-Osmosys <vivek.g@osmosys.co>
2022-04-26 18:41:59 +0530

    Added scripts

--- RerunLastSnapshot/sources.js
+++ RerunLastSnapshot/sources.js
@@ -1,46 +1,49 @@
 module.exports = [
+    {
+        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/core_details/sources/target_us/home"
+    },
     // {
     //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/core_details/sources/linella_md/snapshots"
     // },
     // {
     //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf_beta/collections/core/sources/seven_now_23462"
     // },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nettoshop_ch/snapshots/d6f3f9c2-d884-4c95-a05e-d02d058e3bf6"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/danmurphys_au/snapshots/c6d0c87a-d662-49be-a2f4-b661cae9e348/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/manor_ch/snapshots/bccdd068-b68e-4048-bedd-b484b0fb3754/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nettoshop_fr/snapshots/a1cd71eb-390f-472c-b97a-063a4b9f9b30/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/migro_ch/snapshots/91902c70-9433-4508-a0df-61353b2fb8fd"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/migros_fr_ch/snapshots/7a805133-33bb-418f-8527-dca631d32614/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/microspot_ch/snapshots/6cf45fc7-94f5-4163-96e8-128184a13828/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/jumia_ng/snapshots/5769c92b-48e5-457e-98d7-644320ef3c5f/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/sunstore_ch/snapshots/5535172a-d0d1-4ea0-aee7-6fc4c5f8fe69/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nespresso_ch/snapshots/52c2c285-91e3-4174-bb33-f95013d414e3/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/microspot_fr/snapshots/444399ff-3f5b-46a1-a614-8ad15613d754/home"
-    },
-    {
-        url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/redzac_at/snapshots/31cec4c8-40b7-4dca-950d-5806095a17cb/home"
-    },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nettoshop_ch/snapshots/d6f3f9c2-d884-4c95-a05e-d02d058e3bf6"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/danmurphys_au/snapshots/c6d0c87a-d662-49be-a2f4-b661cae9e348/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/manor_ch/snapshots/bccdd068-b68e-4048-bedd-b484b0fb3754/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nettoshop_fr/snapshots/a1cd71eb-390f-472c-b97a-063a4b9f9b30/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/migro_ch/snapshots/91902c70-9433-4508-a0df-61353b2fb8fd"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/migros_fr_ch/snapshots/7a805133-33bb-418f-8527-dca631d32614/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/microspot_ch/snapshots/6cf45fc7-94f5-4163-96e8-128184a13828/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/jumia_ng/snapshots/5769c92b-48e5-457e-98d7-644320ef3c5f/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/sunstore_ch/snapshots/5535172a-d0d1-4ea0-aee7-6fc4c5f8fe69/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/nespresso_ch/snapshots/52c2c285-91e3-4174-bb33-f95013d414e3/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/microspot_fr/snapshots/444399ff-3f5b-46a1-a614-8ad15613d754/home"
+    // },
+    // {
+    //     url: "https://workbench.import.io/orgs/ascential/projects/digital_shelf/collections/menu___search/sources/redzac_at/snapshots/31cec4c8-40b7-4dca-950d-5806095a17cb/home"
+    // },
 ]
 
 /* 
